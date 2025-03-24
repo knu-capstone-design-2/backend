32d5ffbbc8aa59881843015117a4dd672804ef3d
redbeanbread031 <readbeanbread031@gmail.com>
2025-03-24 23:37:47 +0900

    초기화: 새로운 프로젝트 코드로 변경

---
+++ src/main/java/com/example/monitoring/MonitoringApplication.java
@@ -0,0 +1,15 @@
+package com.example.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringApplication.class, args);
+
+		System.out.println("hello! spring!");
+	}
+
+}
---
+++ src/main/java/com/example/monitoring/model/MonitoringModel.java
@@ -0,0 +1,5 @@
+package com.example.monitoring.model;
+
+public class MonitoringModel {
+
+}
