a1fa1cfe7e845c91f8eed75e28be6fc59356e130
chaechaekim <chaeeun7478@naver.com>
2025-03-26 04:03:42 +0900

    Add method to fetch container CPU usage and improve tests

--- src/test/java/com/example/monitoring/MonitoringApplicationTests.java
+++ src/test/java/com/example/monitoring/MonitoringApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.monitoring;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class MonitoringApplicationTests {
